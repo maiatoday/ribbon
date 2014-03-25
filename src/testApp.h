afec026cbefe19f70ca1f177a411c7688a68d834
maia grotepass <maiatoday@gmail.com>
2014-03-25 22:45:19 +0200

    add image loading from web example

--- src/testApp.h
+++ src/testApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofURLFileLoader.h"
 
 class testApp : public ofBaseApp{
 
@@ -8,6 +9,7 @@ class testApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
+		void exit();
 
 		void keyPressed  (int key);
 		void keyReleased(int key);
@@ -18,5 +20,10 @@ class testApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+
+		void urlResponse(ofHttpResponse & response);
+
+		ofImage img;
+		bool loading;
 		
 };
